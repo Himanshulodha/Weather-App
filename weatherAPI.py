d9c1fad97f8d7b2bf32a8c72b63c333ec87ce293
Himanshu Lodha <93383843+Himanshulodha@users.noreply.github.com>
2024-02-19 01:35:21 +0530

    weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,61 +1,11 @@
 import requests
 import json  #json is builtin module 
 import win32com.client as wincom
-import time
-import re  # Import regular expression module
-
-
-city = input("Enter the name of the city\n")
-
-url = f"https://api.weatherapi.com/v1/current.json?key=d14e54cd15d34e659f2175351241402&q={city}"
-
-
-r = requests.get(url)
-# print(r.text)
-Weather_dic = json.loads(r.text)  # It is function and its loads strings
-w = Weather_dic["current"]["temp_c"]
-
-speak = wincom.Dispatch("SAPI.SpVoice") #You can then create the voice dispatcher object,
-
-# text = f"The current weather in {city} is {w} degrees" # and send it the text you want to read,
-# speak.Speak(text)
-
-
-
-while True:
-    # Assuming 'speak.Speak(text)' is a text-to-speech function, and 'city' and 'w' are defined elsewhere in your code
-    text = f"The current weather in {city} is {w} degrees"
-    speak.Speak(text)
-    print(text)
-
-    # Ask the user if they want to repeat, change the city, change the temperature, or exit
-    user_input = input("Press 'Enter' to repeat, type 'exit' to stop, "
-                       "type 'city' to enter a new city: ")
-
-    # Check user input
-    if user_input.lower() == 'e':
-        break
-    elif user_input.lower() == 'c':
-        # Update 'city' with the new input for the next iteration
-        city = input("Enter a new city: ")
-        # Validate that the new temperature is a number
-        if w.match(r'^-?\d+(?:\.\d+)?$', city):
-           w = float(city)
-           text = f"The new city {city} is not found"
-           speak.Speak(text)
-    elif user_input:
-        print("Invalid input. Please enter 'exit', 'city' or press 'Enter' to repeat.")``
-
-
-
-import requests
-import json  #json is builtin module 
-import win32com.client as wincom
 
 def get_weather(city):  #we using multiple function for different purpose 
     url = f"https://api.weatherapi.com/v1/current.json?key=d14e54cd15d34e659f2175351241402&q={city}"
     r = requests.get(url)
-    return json.loads(r.text)
+    return json.loads(r.text)  # It is function and its loads strings
 
 def speak_weather(city, temperature):
     speak = wincom.Dispatch("SAPI.SpVoice")
